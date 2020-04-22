57056eb4a4c5adcd84fc836b1ea29d58b0c01e7f
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2020-04-22 14:56:47 -0400

    added comments

--- Hello_LCD/Hello LCD.ino
+++ Hello_LCD/Hello LCD.ino
@@ -1,22 +1,27 @@
-#include <LiquidCrystal.h>
+// Hello LCD
+// Harriet Novak
+// Goal: Get an LCD screen to display "hello, world"
 
 
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+#include <LiquidCrystal.h> //lcd library
+
+
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12); // Pins for RS, EN, DB4, DB5, DB6, DB7
 
 void setup () {
 
 
-lcd.begin(16, 2);
+lcd.begin(16, 2); // screen has 16 columns and 2 rows
 
-lcd.print("hello, world!");
+lcd.print("hello, world!"); // LCD screen prints 
 
  }
 
 
 void loop () {
 
-lcd.setCursor(0, 1);
+lcd.setCursor(0, 1); // Puts the cursor to the first spot in the second row, starts at 0
 
-lcd.print(millis()/1000);
+lcd.print(millis()/1000); // prints number of seconds
 	
 }
